fa42e578e98a558449d080ef9c46fb08ffd97b1e
Tiago Alcantara <tiagofacampos@gmail.com>
2021-04-14 15:48:50 -0300

    Finished guide

---
+++ src/main/kotlin/br/com/zup/edu/tiago/HomeController.kt
@@ -0,0 +1,18 @@
+package br.com.zup.edu.tiago
+
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.rules.SecurityRule
+import io.micronaut.views.View
+
+@Controller
+class HomeController {
+
+    @Secured(SecurityRule.IS_ANONYMOUS)
+    @View("home")
+    @Get
+    fun index(): Map<String, Any>{
+        return HashMap()
+    }
+}
